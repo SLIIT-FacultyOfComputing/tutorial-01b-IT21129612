cfade9888ccaa9850ecfa1122eb48cf2b13eab1e
it21129612 <87840790+IT21129612@users.noreply.github.com>
2021-07-23 03:04:10 +0000

    i completed the 1st question

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,17 @@
 #include <stdio.h>
 
 int main() {
+  int sub1;
+  int sub2;
+  int sum;
+  float avg;
+
+  sub1 = 50;   //assume the value to the sub1//
+  sub2 = 40;   //assume the value to the sub2//
+  sum = sub1 + sub2;
+  avg = (sum/2);
+  printf("The average is %f\n:",avg);
+
   
   return 0;
 }
